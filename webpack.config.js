a584f205fe1e781aaf88e86601955800a1e3ac65
Collins Muriuki <murerwacollins@gmail.com>
2020-04-16 12:16:14 +0300

    Configure babel and webpack

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  module: {
+    // define whatwebpack needs to do based on the files it encounters
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        // tells webpack what loader it needs to run
+        // the loader is the helper which converts the code to the actual version
+        // here we use babel
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  }
+}
