b0f54743a472aa19ca65dd1eb88d8f48769d8238
Collins Muriuki <murerwacollins@gmail.com>
2020-04-16 13:00:37 +0300

    Configure html loader and html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   module: {
     // define whatwebpack needs to do based on the files it encounters
@@ -18,7 +20,20 @@ module.exports = {
         use: [
           'style-loader', 'css-loader'
         ]
+      },
+      {
+        test: /\.html$/,
+        use: {
+          loader: 'html-loader'
+        }
       }
+    ],
+    plugins: [
+      // The HtmlWebpackPlugin receives a config object when creating an instance
+      // The object takes the template of the file it uses as the starting point
+      new HtmlWebpackPlugin({
+        template: './index.html'
+      })
     ]
   }
 }
