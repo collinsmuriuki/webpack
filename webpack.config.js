879810e0adba7e359f65bb4b8f5da0022af56830
Collins Muriuki <murerwacollins@gmail.com>
2020-08-09 11:47:10 +0300

    Add webpack server and fix webpack configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,15 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const path = require('path');
 
 module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+  },
   module: {
-    // define whatwebpack needs to do based on the files it encounters
+    // define what webpack needs to do based on the files it encounters
     rules: [
       {
         test: /\.(js|jsx)$/,
@@ -11,29 +18,29 @@ module.exports = {
         // the loader is the helper which converts the code to the actual version
         // here we use babel
         use: {
-          loader: 'babel-loader'
-        }
+          loader: "babel-loader",
+        },
       },
       {
         test: /\.css$/,
         // note that the order in which we insert loaders is important
-        use: [
-          'style-loader', 'css-loader'
-        ]
+        use: ["style-loader", "css-loader"],
       },
       {
         test: /\.html$/,
         use: {
-          loader: 'html-loader'
-        }
-      }
+          loader: "html-loader",
+        },
+      },
     ],
   },
+  mode: "development",
   plugins: [
     // The HtmlWebpackPlugin receives a config object when creating an instance
     // The object takes the template of the file it uses as the starting point
     new HtmlWebpackPlugin({
-      template: './index.html'
-    })
-  ]
-}
+      template: "./index.html",
+    }),
+    new CopyWebpackPlugin(["index.html"]),
+  ],
+};
